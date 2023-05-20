16dbe833b3e3701f181ecd3b56849c9d1425e118
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-05-19 22:04:02 -0300

    updating files

---
+++ database/assignment2.sql
@@ -0,0 +1,31 @@
+
+-- Insert Tony Stark data in account table
+INSERT INTO public.account(account_firstname, account_lastname, account_email, account_password)
+VALUES('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+-- Update Tony Stark account_type
+UPDATE public.account 
+SET account_type = 'Admin'
+WHERE account_id = 1;
+
+-- Delete Tony Stark data
+DELETE FROM public.account WHERE account_id = 1;
+
+-- Replace inv_description GM Hummer data
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
+WHERE inv_model = 'Hummer';
+
+
+-- Select make and model that belong to the "Sport" category
+SELECT inv_make, inv_model, classification_name FROM public.inventory
+INNER JOIN public.classification
+ON public.inventory.classification_id = public.classification.classification_id
+AND public.classification.classification_id = 2;
+
+
+
+-- Replace inv_image and inv_thumbnail with the correct file path
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'), 
+inv_thumbnail = REPLACE (inv_thumbnail, '/images/', '/images/vehicles/');
